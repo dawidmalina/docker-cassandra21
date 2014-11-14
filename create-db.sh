084790b4890ce11b0e436005fd8c3b930d6029de
Dawid Malinowski <d.malinowski@amg.net.pl>
2014-11-14 12:45:55 +0100

    Small fix

--- create-db.sh
+++ create-db.sh
@@ -5,7 +5,7 @@ if [ "x$1" == "x" ]; then
     exit 1;
 fi
 
-[[ $1 =~ ^[-0-9]+$ ]] && echo "Keyspace name should not be a number"; exit 1
+[[ $1 =~ ^[-0-9]+$ ]] && echo "Keyspace name should not be a number" && exit 1
 
 NAME=$1
 IP=$(head -1 /etc/hosts |awk '{print $1}')
