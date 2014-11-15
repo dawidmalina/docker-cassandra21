cd506ad26a63c4522ced51018cf0aee18293f2da
Dawid Malinowski <d.malinowski@amg.net.pl>
2014-11-15 01:14:07 +0100

    Ability to change replication factor

--- create-db.sh
+++ create-db.sh
@@ -5,6 +5,12 @@ if [ "x$1" == "x" ]; then
     exit 1;
 fi
 
+REPLICAS=$2
+if [ "x$REPLICAS" == "x" ]; then
+    echo "Default: 1 replicas"
+    REPLICAS=1
+fi
+
 [[ $1 =~ ^[-0-9]+$ ]] && echo "Keyspace name should not be a number" && exit 1
 
 NAME=$1
@@ -12,4 +18,4 @@ IP=$(head -1 /etc/hosts |awk '{print $1}')
 
 echo "${IP} - keyspace name $1"
 
-cqlsh ${IP} 9042 -e "CREATE KEYSPACE ${NAME} WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'DC1' : 3 }"
+cqlsh ${IP} 9042 -e "CREATE KEYSPACE ${NAME} WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'DC1' : ${REPLICAS} }"
